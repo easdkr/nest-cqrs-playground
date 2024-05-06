ca7c9a109321c6f400e82def21b93fcc8bc67aad
easdkr <easdkr@daum.net>
2024-05-06 17:21:12 +0900

    [feat] Saga에서 발생하는 처리되지 않은 예외 처리 추가

--- src/user/saga/user.saga.ts
+++ src/user/saga/user.saga.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { ICommand, ofType, Saga } from '@nestjs/cqrs';
-import { map, Observable } from 'rxjs';
+import {
+  ICommand,
+  ofType,
+  Saga,
+  UnhandledExceptionBus,
+  UnhandledExceptionInfo,
+} from '@nestjs/cqrs';
+import { map, Observable, Subject, takeUntil } from 'rxjs';
 import { CreateUserEvent } from '../event';
 import { CreateRegisterCouponCommand } from '../command';
 
 @Injectable()
 export class UserSaga {
+  private destroy$ = new Subject<void>();
+
+  constructor(private unhandledExceptionsBus: UnhandledExceptionBus) {
+    this.unhandledExceptionsBus
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((exceptionInfo: UnhandledExceptionInfo) => {
+        console.error('Unhandled exception in saga:', exceptionInfo);
+        // 재시도나 에러 알림 등 처리
+      });
+  }
+
   /** @Saga() 가 있으면 애플리케이션의 이벤트 스트림을 구독하고 `ofType`으로 처리할 이벤트를 선택  */
   @Saga()
   public userCreated(event$: Observable<any>): Observable<ICommand> {
@@ -14,5 +31,10 @@ export class UserSaga {
       ofType(CreateUserEvent),
       map((event) => new CreateRegisterCouponCommand(fakeCouponId, event.id)),
     );
+  }
+
+  onModuleDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
